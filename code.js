ca7fc6761f0ccafe20972f3625804c9cb471a232
Noah Tye <noah@tyes.us>
2012-08-26 18:43:08 -0700

    All tasks now have a one-minute countdown.

--- code.js
+++ code.js
@@ -5,7 +5,11 @@ $(function() {
   var Task = Backbone.Model.extend({
     defaults: {
       'text': 'new task',
-      'time': 10
+      'time': 60 // seconds
+    },
+
+    tick: function() {
+      this.set('time', this.get('time') - 1);
     }
   });
 
@@ -21,10 +25,15 @@ $(function() {
 
 
   var TaskView = Backbone.View.extend({
-    tagName: 'li',
+    tagName: 'tr',
+
+    initialize: function() {
+      this.model.bind('change', this.render, this);
+    },
 
     render: function() {
-      this.$el.html(this.model.get('text'));
+      this.$el.html('<td>' + this.model.get('text') + '</td>' +
+                    '<td>' + this.model.get('time') + '</td>');
       return this;
     }
   });
@@ -40,12 +49,20 @@ $(function() {
     },
 
     addOne: function() {
-      console.log('hi');
-      var task = Tasks.create({ text: this.$('input').val() });
+      var task = Tasks.create({ text: this.$('input').val(), time: 60 });
       var view = new TaskView({ model: task });
       this.$('#tasks').append(view.render().el);
     }
   });
 
   var App = new AppView;
+
+
+  window.setInterval(function() {
+    console.log('---------------------');
+    Tasks.each(function(task) {
+      task.tick();
+      //console.log(task.get('text'), task.get('time'));
+    });
+  }, 1000);
 });
